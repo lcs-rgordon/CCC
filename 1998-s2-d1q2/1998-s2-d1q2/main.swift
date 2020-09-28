e7bf87191ba12b1024ba93a8d3a77fd80e3a7f53
Russell Gordon <rgordon@lcs.on.ca>
2020-09-28 15:10:36 -0400

    Got best guesses to be calculated

--- 1998-s2-d1q2/1998-s2-d1q2/main.swift
+++ 1998-s2-d1q2/1998-s2-d1q2/main.swift
@@ -7,18 +7,46 @@
 
 import Foundation
 
+// probabilities
+let probability: [String.Element: Double] = ["A" : 0.082,
+                                             "B" : 0.015,
+                                             "C" : 0.028,
+                                             "D" : 0.043,
+                                             "E" : 0.127,
+                                             "F" : 0.022,
+                                             "G" : 0.020,
+                                             "H" : 0.061,
+                                             "I" : 0.070,
+                                             "J" : 0.002,
+                                             "K" : 0.008,
+                                             "L" : 0.040,
+                                             "M" : 0.024,
+                                             "N" : 0.067,
+                                             "O" : 0.075,
+                                             "P" : 0.019,
+                                             "Q" : 0.001,
+                                             "R" : 0.060,
+                                             "S" : 0.063,
+                                             "T" : 0.091,
+                                             "U" : 0.028,
+                                             "V" : 0.010,
+                                             "W" : 0.023,
+                                             "X" : 0.001,
+                                             "Y" : 0.020,
+                                             "Z" : 0.001]
+
 // shift a value based on k
 func shift(letter: String.Element, by: Int) -> String {
     
     var shift = Int(letter.asciiValue!) + by
     if shift > 90 {
-//        print("original shift is \(shift)")
+        //        print("original shift is \(shift)")
         shift -= 64
-//        print("after downwards shift is \(shift)")
+        //        print("after downwards shift is \(shift)")
         shift %= 26
-//        print("after division is \(shift)")
+        //        print("after division is \(shift)")
         shift += 64
-//        print("after upward shift is \(shift)")
+        //        print("after upward shift is \(shift)")
     }
     return String(UnicodeScalar(shift)!)
 }
@@ -27,33 +55,52 @@ func shift(letter: String.Element, by: Int) -> String {
 // get number of lines to decipher
 let lines = Int(readLine()!)!
 
+// empty array of translated phrases
+var bestGuesses: [String] = []
+
 // get ciphertext phrases
 for _ in 1...lines {
     
     // get phrase
     let cipherText = readLine()!
     
+    // initialize highest score
+    var highestScore = 0.0
+    var probablePlaintext = ""
+    
     // check a possible plaintext
-    for k in 1...26 {
+    for k in 1...25 {
         
         // build plaintext
+//        print("k is \(k)")
         var plaintext = ""
+        var score = 0.0
         for letter in cipherText {
-            plaintext.append(shift(letter: letter, by: k))
+            if letter != " " {
+                plaintext.append(shift(letter: letter, by: k))
+                score += probability[plaintext.last!]!
+            } else {
+                plaintext.append(" ")
+            }
         }
-        print(plaintext)
+        
+        // check for new highest
+        if score > highestScore {
+            highestScore = score
+            probablePlaintext = plaintext
+        }
+//        print(plaintext)
+//        print(score)
+        
     }
-    print("===")
+//    print("===")
+    
+    // Save best guess
+    bestGuesses.append(probablePlaintext)
     
 }
 
-
-//
-//if let scalar = UnicodeScalar(i) {
-//
-//    // If we got here, inside the "if let" statement, the Unicode scalar was successfully created
-//    let newLetter = String(scalar) // Convert the scalar into a single letter
-//    print("About to add this letter to the string: \(newLetter)")
-//    builtStringFromLoop += newLetter // Now add the letter to the built string
-//
-//}
+// now print best guesses
+for guess in bestGuesses {
+    print(guess)
+}
